31ce3884ad6261f2247a0fc80bf91f76a71350aa
ViR Dash <virb3e@gmail.com>
2015-02-23 09:31:45 +0200

    Initial commit

---
+++ APlus/Functions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace APlus
+{
+	public static class Functions
+	{
+		static readonly string _server = "http://microcast.mcserver.ws/aplus/service.php";
+
+		public static string Request(NameValueCollection data)
+		{
+			using (var wb = new WebClient())
+			{
+				byte[] response = wb.UploadValues(_server, "POST", data);
+				string result = wb.Encoding.GetString(response);
+
+				return result;
+			}
+		}
+
+		public static string GetMd5(string input)
+		{
+			var md5 = MD5.Create();
+			byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+			StringBuilder stringBuilder = new StringBuilder();
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				stringBuilder.Append(data[i].ToString("x2"));
+			}
+				
+			return stringBuilder.ToString();
+		}
+	}
+}
---
+++ APlus/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace APlus
+{
+	[Activity (Label = "APlus", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		Button _btnLogin;
+		EditText _txtEmail, _txtPassword;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Main);
+
+			_btnLogin = FindViewById<Button> (Resource.Id.btnLogin);
+			_txtEmail = FindViewById<EditText> (Resource.Id.txtEmail);
+			_txtPassword = FindViewById<EditText> (Resource.Id.txtPassword);
+
+			_btnLogin.Click += btnLogin_OnClick;
+		}
+
+		void btnLogin_OnClick(object sender, EventArgs e)
+		{
+			var data = new NameValueCollection();
+			data.Add("login", string.Empty);
+			data.Add("email", _txtEmail.Text);
+			data.Add("password", Functions.GetMd5(_txtPassword.Text));
+
+			string reply = Functions.Request(data);
+			Toast.MakeText (this, reply, ToastLength.Long).Show();
+		}
+	}
+}
+
+
